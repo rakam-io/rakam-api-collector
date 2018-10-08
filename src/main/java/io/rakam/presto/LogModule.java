a7b8b9730b35e9022fe47dca4a6d45063305cfae
Burak Emre Kabakcı <emrekabakci@gmail.com>
2018-10-08 15:32:41 +0300

    Fix issues in logger

--- src/main/java/io/rakam/presto/LogModule.java
+++ src/main/java/io/rakam/presto/LogModule.java
@@ -4,10 +4,13 @@
 
 package io.rakam.presto;
 
-import com.getsentry.raven.jul.SentryHandler;
+import com.google.common.base.Splitter;
 import com.google.inject.Binder;
 import io.airlift.configuration.AbstractConfigurationAwareModule;
 import io.airlift.configuration.Config;
+import io.sentry.Sentry;
+import io.sentry.SentryClient;
+import io.sentry.jul.SentryHandler;
 
 import java.io.IOException;
 import java.net.URL;
@@ -21,33 +24,47 @@ public class LogModule
         extends AbstractConfigurationAwareModule
 {
     private static final String SENTRY_DSN = "https://96145b6c6517416e9bcd24bba96e4433:6f5ba5b727114d5e91694b1755584500@sentry.io/1290995";
+    private static final String RELEASE;
+
+    static {
+        URL gitProps = org.rakam.util.LogUtil.class.getResource("/git.properties");
+
+        if (gitProps != null) {
+            Properties properties = new Properties();
+            try {
+                properties.load(gitProps.openStream());
+            } catch (IOException e) {
+            }
+
+            RELEASE = properties.get("git.commit.id.describe").toString();
+        } else {
+            RELEASE = null;
+        }
+    }
 
     @Override
     public void setup(Binder binder)
     {
         LogManager manager = LogManager.getLogManager();
         LogConfig logConfig = buildConfigObject(LogConfig.class);
-        if (logConfig.getLogActive() && !Arrays.stream(manager.getLogger("").getHandlers()).anyMatch(e -> e instanceof SentryHandler)) {
-            Logger rootLogger = manager.getLogger("");
+        if (logConfig.getLogActive()) {
+            if (!Arrays.stream(manager.getLogger("").getHandlers())
+                    .anyMatch(e -> e instanceof SentryHandler)) {
+                Logger rootLogger = manager.getLogger("");
 
-            SentryHandler sentryHandler = new SentryHandler();
-            sentryHandler.setDsn(SENTRY_DSN);
-            sentryHandler.setTags(logConfig.getTags());
-            sentryHandler.setLevel(Level.SEVERE);
-
-            URL gitProps = LogUtil.class.getResource("/git.properties");
-
-            if (gitProps != null) {
-                Properties properties = new Properties();
-                try {
-                    properties.load(gitProps.openStream());
+                SentryClient client = Sentry.init(SENTRY_DSN);
+                if (logConfig.getTags() != null) {
+                    for (String item : Splitter.on(',').split(logConfig.getTags())) {
+                        String[] split = item.split("=", 2);
+                        client.addTag(split[0], split.length > 1 ? split[1] : "true");
+                    }
                 }
-                catch (IOException e) {
-                }
+                client.setRelease(RELEASE);
 
-                sentryHandler.setRelease(properties.get("git.commit.id.describe").toString());
+                SentryHandler sentryHandler = new SentryHandler();
+                sentryHandler.setLevel(Level.SEVERE);
+                rootLogger.addHandler(sentryHandler);
             }
-            rootLogger.addHandler(sentryHandler);
         }
     }
 
